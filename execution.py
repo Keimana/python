b9d864e3b41e1acd10aa27fb86a37380050667d9
Ronaaaaaan <85689535+Keimana@users.noreply.github.com>
2023-11-10 01:16:20 +0800

    execution.py

--- execution.py
+++ execution.py
@@ -1,13 +1,1 @@
-import tkinter
-import tkinter as tk
-window = tkinter.Tk()
 
-window.title ("Login Form")
-window.configure(bg = '#333333')
-window.title("An image to present after logging in")
-
-icon = tk.PhotoImage(file = "pearl.png")
-label = tk.Label(window, image = icon)
-label.pack()
-
-window.mainloop()
