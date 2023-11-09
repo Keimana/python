603a697d957cc402fe73c4f98416cf05c2b997fe
Ronaaaaaan <85689535+ronanbaje@users.noreply.github.com>
2023-11-09 22:34:53 +0800

    LoginForm.py

--- LoginForm.py
+++ LoginForm.py
@@ -1,4 +1,4 @@
-import execution
+from tkinter import *
 
 import tkinter as tk
 import tkinter.messagebox
@@ -8,19 +8,28 @@ window.geometry("800x300")
 window.configure(bg = '#333333')
 
 #execfile to another python script
-#def imgtab():
-    #execfile("")
+
 #Account Validation:
 def login():
-    username = "admin"
-    password = "12345"
+    username = "1"
+    password = "1"
     if user_entry.get()==username and password_entry.get()==password:
-           tkinter.messagebox.showinfo("Login Successfully", "Success!")
-           frame.destroy()
-           
-           
+        tkinter.messagebox.showinfo("Login Successfully", "Success!")  
+        frame.destroy()
+        window.title ("Login Form")
+        window.geometry("1024x1024")
+
+        window.configure(bg = '#333333')
+
+        window.title("An image to present after logging in")
+
+        icon = tk.PhotoImage(file = "pearl.png")
+        label = tk.Label(window, image = icon)
+        label.pack()
+        window.mainloop()
     else:
-         tkinter.messagebox.showinfo("Invalid Credentials", "Invalid Username or Password")
+        tkinter.messagebox.showinfo("Invalid Credentials", "Invalid Username or Password")
+
 #Show Password Function
 def show_password():
 
